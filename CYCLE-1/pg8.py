c2bc94eb62228f6a5e7d6081d7f79b23a6335c74
jophy-mj <74651662+jophy-mj@users.noreply.github.com>
2021-11-29 11:28:53 +0530

    Add files via upload

---
+++ CYCLE-1/pg8.py
@@ -0,0 +1,13 @@
+week_list=['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+week_set=set(week_list)
+week_tuple=tuple(week_list)
+print(week_list)
+print(week_set)
+print(week_tuple)
+print("Type of week_list is ",type(week_list))
+print("Type of week_set is ",type(week_set))
+print("Type of week_tuple is ",type(week_tuple))
+
+#new_dict = dict(zip(list_keys,week_set))
+#print(new_dict)
+#print(type(new_dict))
